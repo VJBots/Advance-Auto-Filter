aad18ae8266f8988b7af459658d64a3fbee18e61
Karuna200 <83528433+Karuna200@users.noreply.github.com>
2022-11-15 12:34:31 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,4 +1,4 @@
-#https://github.com/AM-ROBOTS
+# https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
 from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
 
