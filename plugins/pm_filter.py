e74bbdfb48e9dd27199ac6eaa660d483b6b5b4a2
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-11-15 17:16:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -165,7 +165,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('𝚃𝙷𝙸𝚂 𝙼𝙾𝚅𝙸𝙴 I𝚂 𝙽𝙾𝚃 𝚈𝙴𝚃 𝚁𝙴𝙻𝙴𝙰𝚂𝙴𝙳 𝙾𝚁 𝙰𝙳𝙳𝙴𝙳 𝚃𝙾 𝙳𝙰𝚃𝚂𝙱𝙰𝚂𝙴 💌')
+            k = await query.message.edit('This Movie Isn't Available In Our Public Group , If It's Released Digitally You'll Get It In Our [VIP Group ✨](https://t.me/MrperfectOffcial/39)')
             await asyncio.sleep(10)
             await k.delete()
 
