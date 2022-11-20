19142b64a6aacb7f8a44df9929fc8387c193a38f
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-11-20 17:04:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/KarunaTG/Nataliya.git /Nataliya
+  git clone https://github.com/KarunaTG/EKSH.git /EKSH
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Nataliya
+  git clone $UPSTREAM_REPO /EKSH
 fi
-cd /Nataliya
+cd /EKSH
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
