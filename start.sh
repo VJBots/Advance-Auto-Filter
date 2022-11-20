e137ee3d8186a262d2ea6d844c3431b064e03fd9
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-11-20 17:14:31 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Eva
 fi
-cd /EvaMaria
+cd /Eva
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
