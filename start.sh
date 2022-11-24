df30c69c93a60603cc2a2a3bc9416ef4d85467f5
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-11-24 20:51:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/KarunaTG/EKSH /Eva
+  git clone https://github.com/KarunaTG/Easysky2 /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
