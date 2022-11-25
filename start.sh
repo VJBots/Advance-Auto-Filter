ac009c6f3ce6592376880f408edce17b9c28363f
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-11-25 09:22:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/KarunaTG/Easysky2 /Easysky2
+  git clone https://github.com/KarunaTG/Tnlink /Tnlink
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Easysky2
+  git clone $UPSTREAM_REPO /Tnlink
 fi
-cd /Easysky2
+cd /Tnlink
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
