7dc598d13e9953817cba6777051d3c0db756f2ed
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-11-28 09:18:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://Clicksfly.com/api'
+    url = f'https://tnlink.in/api'
     params = {'api': SHORTNER_API,
               'url': link,
               }
