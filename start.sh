b9c264b02af6a34de931e7d0d69221e37dbbe343
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-11-28 09:17:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/KarunaTG/ClicksFly /Clicksfly
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /ClicksFly
 fi
-cd /DQ-The-File-Donor
+cd /ClicksFly
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
