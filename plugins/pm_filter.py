f6d06da64fcd2eed53777abbb23373bca124c174
KarunaTG <109456795+KarunaTG@users.noreply.github.com>
2022-12-13 09:28:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>If You Want Any Movie, Series Please Join Our Request Groups /n /n ❄️ Request Group: [Click Here](https://t.me/TVSeriesCW/2103)</b>")
+    await message.reply_text("<b>If You Want Any Movie, Series Please Join Our Request Groups \n \n 🔆Request Group: [Click Here](https://t.me/TVSeriesCW/2103)</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
