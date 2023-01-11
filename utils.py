c6e5185adf4beefcfd247c7b0532dea2d38058f5
EasyIce <120899174+IamEasyIce@users.noreply.github.com>
2023-01-11 23:22:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -439,7 +439,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://tnlink.in/api'
+    url = f'https://easysky.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
