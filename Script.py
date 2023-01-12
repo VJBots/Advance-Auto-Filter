40016dc4a01f6fb7ecc643da67f63593de15fa47
EasyIce <120899174+IamEasyIce@users.noreply.github.com>
2023-01-12 20:44:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -161,15 +161,14 @@ Owner Will Update The Movie Within 24Hour"""
     NORSLTS = """
 ★ #Auto_Request ★
 
-🔆<b>Request</b> : 🎗️`{}`🎗️
-♦️<b>Requested By : {}</b>
-♦️<b>User ID : {}</b>"""
+🔆 <b>Request</b> : 🎗️`{}`🎗️
+♦️ <b>Requested By : {}</b>
+♦️ <b>User ID</b> : `{}` """
 
     CAPTION = """
 🎗<b>[{file_name}](https://t.me/+3cfeesNk_cg3MGQ9)</b>🎗 \n\n🔆 <i><b>Movies Group » [@HeavenOfMovies](https://t.me/+_HgfqNy7N4owNTI1)</i></b> \n🔆 <b><i>Series Group » [@HeavenOfSeries](https://t.me/+UbT__2jSGfgzMGM1)</b></i>"""
 
-    IMDB_TEMPLATE_TXT = """
-<i><b>🎗️ {title} {year} \n━━━━━━━━━━━━━━━━━ \n🎭 Genre › {genres} \n⭐ Rating › {rating} \n•────────────────• \n🔆彡[ @TVSeriesCW ]彡🔆</b> </i>"""
+    IMDB_TEMPLATE_TXT = """<i><b>🎗️ {title} {year} \n━━━━━━━━━━━━━━━━━ \n🎭 Genre › {genres} \n⭐ Rating › {rating} \n♦️ Story › {plot}\n•────────────────• \n🔆彡[ @TVSeriesCW ]彡🔆</b> </i>"""
     
     ALL_FILTERS = """
 <b>Hᴇʏ {}, Tʜᴇsᴇ ᴀʀᴇ ᴍʏ ᴛʜʀᴇᴇ ᴛʏᴘᴇs ᴏғ ғɪʟᴛᴇʀs.</b>"""
