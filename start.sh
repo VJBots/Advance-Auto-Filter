fc0afe99adffee47948a639bc7dfcf01315f5fd7
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-01-21 13:01:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BichuPg/Test-2.0 /Test-2.0
+  git clone https://github.com/Vijay14958/url-Shortner-3 /url-Shortner-3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Test-2.0
+  git clone $UPSTREAM_REPO /url-Shortner-3
 fi
-cd /Test-2.0
+cd /url-Shortner-3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
