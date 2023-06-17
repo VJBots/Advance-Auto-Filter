bf5c8a253dd5b4ff772e82da7bba71760a09bc46
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-06-17 08:42:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,8 +47,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "vjbotsss")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjbotsss')
 
 # Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'tC4K2eEKOJeUKW4vVNyoyvgK2jx1')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'moneykamalo.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
