517120cc879f7f99d6e836b229000df6a072ed59
VJBots <122880036+VJBots@users.noreply.github.com>
2023-08-21 16:42:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vijay14958/url-Shortner- /url-Shortner-
+  git clone VJBots/Advance-Auto-Filter /Advance-Auto-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /url-Shortner-
+  git clone $UPSTREAM_REPO /Advance-Auto-Filter
 fi
-cd /url-Shortner-
+cd /Advance-Auto-Filter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
